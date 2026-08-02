db9dc07798e9f0ae70c5722442b39413997998d8
Geeta <geeta.pradhan3009@gmail.com>
2026-08-02 14:43:35 +0530

    first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Workspace\OpenCartV121
+mvn test
---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,55 @@
+package testBase;
+
+import java.time.Duration;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BaseClass {
+public WebDriver driver;
+public Logger logger;
+	
+	@BeforeClass
+	public void setup()
+	{
+	
+		driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().window().maximize();
+	}
+	
+	@AfterClass
+	public void teardown()
+	{
+		driver.quit();	
+	}
+	
+	public String randomeString()
+	{
+		String generatedString=RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+	
+	public String randomeNumber()
+	{
+		String generatedNum=RandomStringUtils.randomNumeric(10);
+		return generatedNum;
+	}
+	
+	public String randomeAlphaNumeric()
+	{
+		String generatedString =RandomStringUtils.randomAlphabetic(0);
+		String randomeAlphaNumeric=RandomStringUtils.randomNumeric(5);
+		return(generatedString+"@"+randomeAlphaNumeric);
+	}
+
+
+}
